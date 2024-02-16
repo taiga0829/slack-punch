d7f4eb39636fb7bd9197e0b935bbbeee0f8a49e8
taiga0829 <taiga.mizutani829@gmail.com>
2024-02-16 10:46:36 +0100

    add modify_work_execution

---
+++ modify_work_execution/modalOpen.js
@@ -0,0 +1,118 @@
+/**
+ * モーダルBlocks
+ */
+const createModalView = () => {
+  return {
+    "type": "modal",
+    "title": {
+      "type": "plain_text",
+      "text": "👊👊Modify Punch👊👊",
+      "emoji": true
+    },
+    "submit": {
+      "type": "plain_text",
+      "text": "Submit",
+      "emoji": true
+    },
+    "close": {
+      "type": "plain_text",
+      "text": "Cancel",
+      "emoji": true
+    },
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "choose the date when you want to modify data."
+        }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "datepicker",
+          "initial_date": "2024-01-09",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select a date",
+            "emoji": true
+          },
+          "action_id": "timepicker-action",
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Rest time",
+          "emoji": true
+        },  
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "timepicker",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select start time",
+            "emoji": true
+          },
+          "action_id": "timepicker-action"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Select start time",
+          "emoji": true
+        },
+        
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "timepicker",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select finish time",
+            "emoji": true
+          },
+          "action_id": "timepicker-action"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Select finish time",
+          "emoji": true
+        },
+        
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "timepicker",
+          "initial_time": "00:00",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select time",
+            "emoji": true
+          },
+          "action_id": "timepicker-action"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Rest time",
+          "emoji": true
+        },
+      }, 
+      {
+        "block_id": "my_block_id",
+        "type": "input",
+        "optional": true,
+        "label": {
+          "type": "plain_text",
+          "text": "Select a channel to post the result on",
+        },
+        "element": {
+          "action_id": "my_action_id",
+          "type": "conversations_select",
+          "response_url_enabled": true,
+        },
+      },
+    ]
+  }
+};
